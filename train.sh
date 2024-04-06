027440fe7a5fd8e24d24c7dda2e88d112c7a12bc
liuzhifei <liuzhifei1226@163.com>
2024-04-06 20:19:27 +0800

    update

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for i in $(seq 1 5);
 do seed=$(( ( RANDOM % 10000 )  + 1 ));
-python3 SMVulDetector.py --model gcn_modify --seed $seed --epochs 100 > log/smartcheck_"$i".log;
+python3 SMVulDetector.py --dataset LOOP_CORENODES_1317 --model gcn_modify --seed $seed --epochs 100 > log/smartcheck_loop_"$i".log;
 done
 
