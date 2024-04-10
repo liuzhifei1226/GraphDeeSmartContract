a28e47d45d836c8db925390b1525bf87f917efdd
liuzhifei <liuzhifei1226@163.com>
2024-04-10 10:26:35 +0800

    update

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for i in $(seq 1 5);
-do seed=$(( ( RANDOM % 10000 )  + 1 ));
-python3 SMVulDetector.py --dataset REENTRANCY_CORENODES_1671 --model gat --seed $seed --epochs 200 > GAT_log/smartcheck_re_"$i".log;
-done
+#for i in $(seq 1 5);
+#do seed=$(( ( RANDOM % 10000 )  + 1 ));
+python3 SMVulDetector.py --dataset REENTRANCY_CORENODES_1671 --model gat  --epochs 200 > GAT_log/smartcheck_re.log;
+#done
 
