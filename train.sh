8691a36c6d92c16ed8cd75a0549f6b7983478c6b
liuzhifei <liuzhifei1226@163.com>
2024-06-06 15:23:46 +0800

    update test

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #for i in $(seq 1 5);
 #do seed=$(( ( RANDOM % 10000 )  + 1 ));
-python3 SMVulDetector.py --dataset REENTRANCY_CORENODES_1671 --model gcn_modify  --epochs 200 > GCN_modify_log/smartcheck_re.log;
+python3 SMVulDetector.py --dataset REENTRANCY_FULLNODES_1671 --model gcn_modify  --epochs 200 > train_log/GCN_modify_log/smartcheck_re.log;
 #done
 
