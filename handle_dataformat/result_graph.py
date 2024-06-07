68421a5488937f45ff611c9e8000258eb20826a0
liuzhifei <liuzhifei1226@163.com>
2024-06-07 10:35:09 +0800

    update test

---
+++ handle_dataformat/result_graph.py
@@ -0,0 +1,32 @@
+import re
+import matplotlib.pyplot as plt
+
+train_epochs = []
+loss_values = []
+accuracy_values = []
+
+# 从文件中读取训练数据
+file_path = "../train_log/GCN_modify_log/smartcheck_re2.log"  # 存储训练数据的文件路径
+with open(file_path, 'r') as file:
+    for line in file:
+        # 使用正则表达式匹配loss和accuracy值
+        match = re.search(r'Loss: (\d+\.\d+)', line)
+        if match:
+            loss_values.append(float(match.group(1))*100)
+        match = re.search(r'Accuracy: (\d+\.\d+)%', line)
+        if match:
+            accuracy_values.append(float(match.group(1)))
+
+# 生成训练轮次
+train_epochs = list(range(1, len(loss_values) + 1))
+
+# 绘制图表
+plt.figure(figsize=(10, 5))
+plt.plot(train_epochs, loss_values, label='Loss')
+plt.plot(train_epochs, accuracy_values, label='Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Value')
+plt.title('Training Loss and Accuracy')
+plt.legend()
+plt.grid(True)
+plt.show()
