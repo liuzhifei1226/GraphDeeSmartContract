3006c5e4cddaa392a9ec8fd1c25513716e074ab5
liuzhifei <liuzhifei1226@163.com>
2024-06-14 10:09:29 +0800

    update

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #for i in $(seq 1 5);
 #do seed=$(( ( RANDOM % 10000 )  + 1 ));
-nohup python3 SMVulDetector.py --dataset REENTRANCY_FULLNODES_1671 --model gcn_modify --dropout 0.3 --lr 0.001 --epochs 200 -b 64 > train_log/GCN_modify_log/re.log 2>&1 &
+nohup python3 SMVulDetector.py --dataset REENTRANCY_FULLNODES_1671 --model gcn_modify --dropout 0.5 --lr 0.0005 --seed 100 --epochs 200 -b 128 > train_log/GCN_modify_log/re.log 2>&1 &
 nohup python3 SMVulDetector.py --dataset REENTRANCY_CORENODES_1671 --model graphsage --epochs 200 > train_log/graphsage/2024.6.13.log 2>&1 &
 
 #done
