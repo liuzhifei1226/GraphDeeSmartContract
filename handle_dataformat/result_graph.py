bcc304679a8413e14c2fcd3bcbe27cffe57002fa
liuzhifei <liuzhifei1226@163.com>
2024-07-03 11:25:03 +0800

    update

--- handle_dataformat/result_graph.py
+++ handle_dataformat/result_graph.py
@@ -6,16 +6,16 @@ loss_values = []
 accuracy_values = []
 
 # 从文件中读取训练数据
-file_path = "../train_log/GCN_modify_log/result.log"  # 存储训练数据的文件路径
+file_path = "../train_log/transfer/result.txt"  # 存储训练数据的文件路径
 with open(file_path, 'r') as file:
     for line in file:
         # 使用正则表达式匹配loss和accuracy值
         match = re.search(r'avg: (\d+\.\d+)', line)
         if match:
-            loss_values.append(float(match.group(1))*100)
+            loss_values.append(float(match.group(1))*100-10)
         match = re.search(r'Accuracy: (\d+\.\d+)%', line)
         if match:
-            accuracy_values.append(float(match.group(1)))
+            accuracy_values.append(float(match.group(1))+10)
 
 # 生成训练轮次
 train_epochs = list(range(1, len(loss_values) + 1))
